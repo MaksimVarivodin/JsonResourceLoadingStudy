36c8ab8cffc9ca3bce1ab0e53e65c23988bafeb7
MaksymVarivodin <90093919+MaksimVarivodin@users.noreply.github.com>
2026-05-03 03:05:04 +0200

    Refactor resource loading and update project configurations

--- JsonResourceLoadingStudy/JournalistLib.hpp
+++ JsonResourceLoadingStudy/JournalistLib.hpp
@@ -1,8 +1,12 @@
 ﻿#pragma once
 #include <json/json.h>
 #include "RResourceManager/ISerializable.hpp"
-#include "RResourceManager/Resource.hpp"
 #include "RResourceManager/JsonLoadable.hpp"
-#include "RResourceManager/RResource.hpp"
 #include "RResourceManager/ParserFactory.hpp"
 #include "RResourceManager/PolymorphicResource.hpp"
+#include "RResourceManager/Resource.hpp"
+
+#include "RResourceManager/RResource.hpp"
+
+#include "RResourceManager/RResourceManager.hpp"
+#include "RResourceManager/RType.hpp"
